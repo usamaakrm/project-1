a35bef79927f8ccdde6ae78cecc248b94ee912e1
Usamah Akram <usamah@techalchemy.co>
2024-05-04 11:16:04 +0530

    first-change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+"use strict";
+
+const accordionItemHeaders = document.querySelectorAll(
+	".accordion-item-header"
+  );
+  
+  accordionItemHeaders.forEach((accordionItemHeader) => {
+	accordionItemHeader.addEventListener("click", (event) => {
+	  // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+  
+	  const currentlyActiveAccordionItemHeader = document.querySelector(
+		".accordion-item-header.active"
+	  );
+	  if (
+		currentlyActiveAccordionItemHeader &&
+		currentlyActiveAccordionItemHeader !== accordionItemHeader
+	  ) {
+		currentlyActiveAccordionItemHeader.classList.toggle("active");
+		currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+	  }
+	  accordionItemHeader.classList.toggle("active");
+	  const accordionItemBody = accordionItemHeader.nextElementSibling;
+	  if (accordionItemHeader.classList.contains("active")) {
+		accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+	  } else {
+		accordionItemBody.style.maxHeight = 0;
+	  }
+	});
+  });
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    var logo = $(".site_logo")
+    if(scroll>=50){
+      logo.addClass('word').removeClass('logo')
+    }else{
+      if(logo.hasClass('word')){
+        logo.addClass('logo').removeClass('word')
+      }
+    }
+  })
+
+
+
 $(function () {
   
     var controller = new ScrollMagic.Controller();
