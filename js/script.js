3fb2b71fef4016d7b19f1387a09c5dff85b877f3
Usamah Akram <usamah@techalchemy.co>
2024-05-08 23:12:06 +0530

    log

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const accordionItemHeaders = document.querySelectorAll(
   var img3 = document.getElementById("img3")
   var img4 = document.getElementById("img4")
   window.addEventListener('scroll', function(e) {
-    if( document.documentElement.scrollTop < 2650){
+    if( document.documentElement.scrollTop < 3530){
       img1.style.display = "block";
       img2.style.display = "none";
       img3.style.display = "none";
@@ -41,7 +41,7 @@ const accordionItemHeaders = document.querySelectorAll(
     }
   })
   window.addEventListener('scroll', function(e) {
-    if( document.documentElement.scrollTop > 2650){
+    if( document.documentElement.scrollTop > 3530){
       img2.style.display = "block";
       img1.style.display = "none";
       img3.style.display = "none";
@@ -50,7 +50,7 @@ const accordionItemHeaders = document.querySelectorAll(
   })
   
   window.addEventListener('scroll', function(e) {
-    if( document.documentElement.scrollTop > 4090){
+    if( document.documentElement.scrollTop > 4570){
       img3.style.display = "block";
       img1.style.display = "none";
       img2.style.display = "none";
@@ -58,7 +58,7 @@ const accordionItemHeaders = document.querySelectorAll(
     }
   })
   window.addEventListener('scroll', function(e) {
-    if( document.documentElement.scrollTop > 5160){
+    if( document.documentElement.scrollTop > 6040){
       img4.style.display = "block";
       img1.style.display = "none";
       img2.style.display = "none";
@@ -67,7 +67,7 @@ const accordionItemHeaders = document.querySelectorAll(
   })
 
   window.addEventListener('scroll', function(e) {
-    if( document.documentElement.scrollTop > 4470){
+    if( document.documentElement.scrollTop > 6570){
       function counter(){   
         $('.count').each(function(){
             if ($(this).hasClass('start')){
